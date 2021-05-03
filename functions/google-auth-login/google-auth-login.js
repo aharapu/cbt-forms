f27bfda4085f4265a76a33bfe2abeb76112e90eb
Valentin Harapu <valentin.harapu@vspartners.us>
2021-05-03 08:17:00 +0300

    test netliyfunc mongodb connection

--- functions/google-auth-login/google-auth-login.js
+++ functions/google-auth-login/google-auth-login.js
@@ -1,19 +1,31 @@
 const {OAuth2Client} = require('google-auth-library');
+const { MongoClient } = require("mongodb");
+
+const googleClient = new OAuth2Client(process.env.REACT_APP_GOOGLE_CLIENT_ID);
+const mongoClient = new MongoClient(process.env.MONGODB_URL);
 
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 const handler = async (event) => {
   try {
     const { idToken } = JSON.parse(event.body);
     
-    const client = new OAuth2Client(process.env.REACT_APP_GOOGLE_CLIENT_ID);
-
-    const ticket = await client.verifyIdToken({
+    const ticket = await googleClient.verifyIdToken({
       idToken,
       audience: process.env.REACT_APP_GOOGLE_CLIENT_ID
     });
 
     const payload = ticket.getPayload();
     const userId = payload['sub'];
+
+    await mongoClient.connect();
+    const db = mongoClient.db('cbt-forms-users');
+    const collection = db.collection('cbt-forms-users');
+    const doc = { name: payload.name, email: payload.email };
+
+    const result = await collection.insertOne(doc);
+    console.log(
+      `${result.insertedCount} documents were inserted with the _id: ${result.insertedId}`,
+    );
 
     return {
       statusCode: 200,
@@ -24,6 +36,8 @@ const handler = async (event) => {
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
+  } finally {
+    await mongoClient.close();
   }
 }
 
